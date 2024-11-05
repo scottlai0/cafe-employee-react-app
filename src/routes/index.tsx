9d798f016355ae131f14b87f2d5fcbca7e25c0d2
scottlai0 <scottlai96@hotmail.com>
2024-11-06 01:29:32 +0800

    formatted index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,13 @@
+import { Divider } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: () => 
     <div>
-      <h1>Hello</h1>
+      <h1>Cafe-Employee Management Portal</h1>
+      <Divider/>
+      <p>This is a experimental project built using Vite + React.</p>
+      <p>API is built with FastAPI</p>
+      <p>Database is a simple SQLite file.</p>
     </div>,
 })
