266354b2f2f07e41d8711327316daeab539722ab
scottlai0 <scottlai96@hotmail.com>
2024-11-06 02:29:09 +0800

    updated with production url

--- src/services/axios-instance.tsx
+++ src/services/axios-instance.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+//const HOST_URL = 'http://localhost:8000/'
+const HOST_URL = 'https://sample-fastapi-1.onrender.com/'
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: HOST_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
